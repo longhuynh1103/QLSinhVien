bd1149f12aef3b4ca0c676aab1764392157fcf96
187PM06719 - Doan Nguyen Thong - K24T-PM1 <thong.187pm06719@vanlanguni.vn>
2021-11-09 22:12:52 +0700

    Add layout liststudent, listsubject, create activityaddstudent,activityinformationstudent, ActivityUpdateStudent,code function ActivityStudent

---
+++ app/src/main/java/com/example/qlsinhvien/ActivityInformationStudent.java
@@ -0,0 +1,14 @@
+package com.example.qlsinhvien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityInformationStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_student);
+    }
+}
