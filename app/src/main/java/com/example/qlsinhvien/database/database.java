b3f6a8abc5606bc92cea4f36eb2a74b2dd495a2f
187PM13890 - Le Hoang Phuong Anh - K24T-PM1 <anh.187pm13890@vanlanguni.vn>
2021-11-09 11:24:24 +0700

    Create Student table in database Create Subject table in database

---
+++ app/src/main/java/com/example/qlsinhvien/database/database.java
@@ -0,0 +1,73 @@
+package com.example.qlsinhvien.database;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.qlsinhvien.model.Student;
+import com.example.qlsinhvien.model.Subject;
+
+public class database extends SQLiteOpenHelper{
+    //Tên database
+    private static String DATABASE_NAME = "studentmanagement";
+    //Bảng môn học
+    private static String TABLE_SUBJECTS = "subject";
+    private static String ID_SUBJECTS = "idsubject";
+    private static String SUBJECT_TITLE = "subjecttitle";
+    private static String CREDITS = "credits";
+    private static String TIME = "time";
+    private static String PLACE = "place";
+    private static int VERSION = 1;
+
+    //Bảng sinh viên
+    private static String TABLE_STUDENT = "student";
+    private static String ID_STUDENT = "idstudent";
+    private static String STUDENT_NAME = "sudentname";
+    private static String SEX = "sex";
+    private static String STUDENT_CODE = "studentcode";
+    private static String DATE_OF_BIRTH = "dateofbirth";
+
+
+
+    private Context context;
+
+    //Tạo bảng môn học
+    private String SQLQuery = "CREATE TABLE "+ TABLE_SUBJECTS +" ( "+ID_SUBJECTS+" INTEGER PRIMARY KEY AUTOINCREMENT, "
+            +SUBJECT_TITLE+" TEXT, "
+            +CREDITS+" INTEGER, "
+            +TIME+" TEXT, "
+            + PLACE+" TEXT) ";
+
+    //Tạo bảng sinh viên
+    private String SQLQuery1 = "CREATE TABLE "+ TABLE_STUDENT +" ( "+ID_STUDENT+" integer primary key AUTOINCREMENT, "
+            +STUDENT_NAME+" TEXT, "
+            +SEX+" TEXT, "
+            +STUDENT_CODE+" TEXT, "
+            +DATE_OF_BIRTH+" TEXT, "
+            +ID_SUBJECTS+" INTEGER , FOREIGN KEY ( "+ ID_SUBJECTS +" ) REFERENCES "+
+            TABLE_SUBJECTS+"("+ID_SUBJECTS+"))";
+
+
+    public database(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Tạo 2 bảng
+        db.execSQL(SQLQuery);
+        db.execSQL(SQLQuery1);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
